4d8c8836a7a475fb9b327f6cb9d158ef46a0d32d
zlaptoppeo <zlaptoppeo@zlaptop.(none)>
2014-03-25 17:38:38 +0100

    new tool to plot networks, need some aesthetics

---
+++ tools/pathway.py
@@ -0,0 +1,246 @@
+#!/usr/bin/python
+#!python
+
+#THIS CREATES A GRAPH WITH GRAPHVIZ
+# FOR ALL THE SPECIES THAT CONTAINS A GIVEN ATOM
+#written by Tommaso Grassi and the KROME team (Mar25,2014)
+
+import sys,os
+from subprocess import call
+from random import random as rand
+
+########################################
+# PLEASE DO NOT MODIFY IF YOU ARE NOT 
+# SURE OF WHAT YOU ARE DOING!
+########################################
+tmpfile = str(1e8+rand()*1e8)+"tmp.dot"
+EMAX = 2 #maximum number of object on label edge. if greater select by degree
+
+
+#check command-line arguments and store them
+if(len(sys.argv)<4):
+	print ("Usage: %s INPUT OUTPUT BASE" % sys.argv[0])
+   	print " where BASE is the atom shared by the species (case sensitive!)"
+	sys.exit()
+
+if(not os.path.isfile(sys.argv[1].strip())):
+    sys.exit("ERROR: input file %s was not found!" % sys.argv[1])
+
+fname = sys.argv[1].strip()
+outfile = sys.argv[2].strip()
+base = sys.argv[3].strip()
+if(fname==outfile):
+    sys.exit("ERROR: input and output are the same file!")
+if(not(".eps" in outfile)):
+    sys.exit("ERROR: output file should end with .eps!")
+
+##############
+class form():
+	reactants = []
+	products = []
+	idx = 0
+	tmin = 0
+	tmax = 0
+	rate = 0
+	fmt = ""
+	nfmt = 0
+	def proc(self,myfmt):
+		self.reactants = []
+		self.products = []
+		self.idx = 0
+		self.tmin = 0
+		self.tmax = 0
+		self.rate = 0
+		self.fmt = myfmt
+		self.nfmt = len(myfmt.split(","))
+		afmt = myfmt.replace("@format:","").lower().split(",")
+		while("r" in afmt):
+			ii = afmt.index("r")
+			self.reactants.append(ii)
+			afmt[ii] = ""
+		while("p" in afmt):
+			ii = afmt.index("p")
+			self.products.append(ii)
+			afmt[ii] = ""
+		if("idx" in afmt):
+			self.idx = afmt.index("idx")
+		if("tmin" in afmt):
+			self.tmin = afmt.index("tmin")
+		if("tmax" in afmt):
+			self.tmax = afmt.index("tmax")
+		if("rate" in afmt):
+			self.rate = afmt.index("rate")
+	def show(self):
+			print self.reactants
+			print self.products
+			print self.idx
+			print self.tmin
+			print self.tmax
+			print self.rate
+			print self.fmt
+
+
+cfmt = form() #object class format
+fh = open(fname,"rb") #open file to read
+fout = open(tmpfile,"w") #open output file
+foundedge = dict() #dictionary key="x -> y", value=[reaction partners]
+foundrea = [] #list of reaction found (for output on screen only)
+
+#start writin digraph file
+fout.write("//DOT file created with PATHWAY of KROME\n")
+fout.write("// see kromepackage.org\n")
+fout.write("digraph pathway{\n")
+fout.write("node [shape=circle];\n")
+
+degree = dict()
+
+print "**************************"
+print "        PATHWAYS!"
+print "**************************"
+print "Reading from "+fname
+#default format
+fmt = "@format:idx,R,R,R,P,P,P,P,Tmin,Tmax,rate"
+cfmt.proc(fmt) #process format
+skip = False
+ifound = 0
+for row in fh:
+	#skip comments and blanks
+	skipThis = False
+	srow = row.strip()
+	if(srow==""): continue
+	if(srow[0]=="#"): skipThis = True
+	if(srow[0:2]=="//"): skipThis = True
+	if("*/" in srow):
+		skip = False
+		skipThis = True
+	if(srow[0:2]=="/*"): 
+		skip = True
+		skipThis = True
+	if(skipThis):
+		continue
+	if(skip): continue
+
+	#process or skip format statement
+	if("@format:" in srow):
+		cfmt.proc(srow)
+		continue
+	if(srow[0]=="@"): continue
+
+	#split line
+	arow = srow.split(",")
+
+	#find reactants
+	rr = []
+	for ir in cfmt.reactants:
+		if(arow[ir].strip()!=""): rr.append(arow[ir])
+
+	#find products
+	pp = []
+	for ip in cfmt.products:
+		if(arow[ip].strip()!=""): pp.append(arow[ip])
+	#check for reaction with more than 3.body
+	if(len(rr)>3): 
+		print "WARNING: "+str(len(rr))+"-body skipped!"
+		continue
+
+	#store degree
+	crhv = []
+	if(len(rr)==1): crhv = ["CR/hv"] #empty reactants means CR or photon
+	for x in rr+pp+crhv:
+		if(x in degree): 
+			degree[x] += 1
+		else:
+			degree[x] = 1
+
+	#search for the atom in reactants and products
+	if(base in rr+pp):
+		irr = 0
+		#loop on reactants
+		for x in rr:
+			#if a reactants has the atom
+			if(base in x):
+				node_start = x #store reactants
+				other_reacts = []
+				#store all other reactants
+				for i in range(len(rr)):
+					if(i!=irr): other_reacts.append(rr[i])
+
+				node_ends = []
+				#store products with the atom
+				for p in pp:
+					if(base in p):
+						node_ends.append(p)
+				#empty reactants are CR or photons
+				if(len(other_reacts)==0): other_reacts = ["CR/hv"]
+				#loop on products
+				for nodee in node_ends:
+					ss = "\""+x+"\" -> \""+nodee+"\"" #edge as key
+					#add reactants
+					if(ss in foundedge):
+						foundedge[ss] += other_reacts
+					else:
+						foundedge[ss] = other_reacts
+
+					#check for reactions to print on screen
+					if([rr,pp] in foundrea): continue
+					ifound += 1
+					print str(ifound)+". "+(" + ".join(rr))+" -> "+(" + ".join(pp))
+					foundrea.append([rr,pp])
+
+			irr += 1 #count reactants
+
+if(ifound==0):
+	sys.exit("ERORR: no reactions found with atom "+base+"!")
+
+#dictionary->list (easier to sort)
+sumdeg = sum(degree.values())
+deg = []
+for k,v in degree.iteritems():
+	deg.append([k,v*100./sumdeg])
+deg = sorted(deg, key=lambda x:x[1],reverse=True)
+#print deg
+
+#prepare dot file
+gotwarn = False
+for k,v in foundedge.iteritems():
+	uniq = []
+	#unique labels on edge
+	for x in v:
+		if(x in uniq): continue
+		uniq.append(x)
+	#print "*********"
+	#print k
+	#print uniq
+	vals = []
+	#if more than EMAX objects on the edge label keeps only the EMAX with highes degree
+	if(len(uniq)>EMAX):
+		tmpu = (", ".join(uniq))
+		for x in uniq:
+			vals.append([x,int(degree[x]*100./sumdeg)])
+		vals = sorted(vals, key=lambda x:x[1],reverse=True)
+		uniq = [x[0] for x in vals[0:EMAX]]
+		print "WARNING: found ("+tmpu+") selected ("+(", ".join(uniq))+")"#,vals
+		gotwarn = True
+	#prepare link with label
+	link = "\t"+k+" [label=\""+(",".join(uniq))+"\"];\n"
+	fout.write(link)
+if(gotwarn): print "To avoid this warning change EMAX in the script."
+fout.write("}\n")
+fout.close()
+
+#run dot to prepare eps file
+fstdout = open(outfile,"w")
+try:
+	call(["dot",tmpfile,"-Teps"],stdout=fstdout)
+except:
+	print "ERROR: problem with dot!"
+	print "You need graphviz to run this script (http://www.graphviz.org/)"
+	sys.exit()
+os.remove(tmpfile) #delete temporary file
+
+
+print "Output written in "+outfile
+print "DONE, bye!"
+
+
+
