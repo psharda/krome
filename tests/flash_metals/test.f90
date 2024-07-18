2066e00c6fe2ae4ae4405b9f167143441e87f0d0
psharda <piyush.suneet@gmail.com>
2024-07-18 11:09:17 +0200

    print heats and cools, use Tdust from dust tables instead of user input

--- tests/flash_metals/test.f90
+++ tests/flash_metals/test.f90
@@ -19,16 +19,16 @@ program test_krome
   real*8::tff,dd,dd1
   real*8::x(krome_nmols),Tgas,dt
   real*8::ntot
-  real*8::Av, NHtot
+  real*8::Av, NHtot, totheat, totcool
 
   !INITIAL CONDITIONS
   krome_redshift = 15d0    !redshift
   ntot = 1d0               !total density, cm-3
   Tgas = 1d2               !temperature, K
 
-  call krome_set_user_Tdust(30d0)
-  call krome_set_user_crate(1d-17)
+  !call krome_set_user_Tdust(30d0)
   call krome_set_user_Av(0d0)
+  call krome_set_user_crate(1d-17)
   call krome_set_user_ionH(0d0)
   call krome_set_user_ionH2(0d0)
   call krome_set_user_ionC(0d0)
@@ -50,8 +50,8 @@ program test_krome
   x(KROME_idx_E)         = 1d-4*ntot
   x(KROME_idx_Hj)        = 1d-4*ntot
   x(KROME_idx_HE)        = 0.0775*ntot
-  x(KROME_idx_Cj)        = 0.927e-4*ntot
-  x(KROME_idx_O)         = 5.68e-4*ntot
+  x(KROME_idx_Cj)        = 0.927d-4*ntot
+  x(KROME_idx_O)         = 3.568d-4*ntot
 
   !set initial density
   dd = ntot
@@ -60,7 +60,7 @@ program test_krome
   open(newunit=unit,file="explore.dat",status="replace")
 
   print *,"solving..."
-  print '(a5,3a11)',"step","n(cm-3)","Tgas(K)", "Av"
+  print '(a5,5a11)',"step","n(cm-3)","Tgas(K)", "Av", "sum_heats", "sum_cools"
 
   !output header
   write(22,*) "#ntot Tgas "//trim(krome_get_names_header())
@@ -98,11 +98,13 @@ program test_krome
      !print some output
      write(22,'(99E17.8e3)') dd,Tgas,x(:)/dd
      if(mod(i,50)==0) then
-        print '(I5,99E11.4)',i,dd,Tgas,Av
+        totheat = krome_get_heating(x(:), Tgas)
+        totcool = krome_get_heating(x(:), Tgas)
+        print '(I5,3E11.3,2E13.6,1E11.3)',i,dd,Tgas,Av,totheat,totcool,(totheat-totcool)*(1.66667-1.)/1.380648e-16/dd
         call krome_print_best_flux(x(:),Tgas,5)
         call krome_explore_flux(x(:),Tgas,unit,dd)
      end if
-
+     !call krome_dump_cooling(x(:),Tgas)
   end do
 
   !close explore data file
