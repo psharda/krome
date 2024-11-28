7f3d581165b8188f8417b703e2fc8232d5524395
Piyush Sharda <psharda@RSAA-43608.local>
2024-11-28 21:17:55 +0100

    initialize Deuterium abundance with cosmic D/H ratio

--- tests/popsicle_semenov_photo_cr_ismEqTest/test_cooling.f90
+++ tests/popsicle_semenov_photo_cr_ismEqTest/test_cooling.f90
@@ -64,6 +64,7 @@ program test_cooling
     x(KROME_idx_HE)        = 0.0775*ntot
     x(KROME_idx_Cj)        = 0.927d-4*zs(jz2)*ntot !C is fully ionized
     x(KROME_idx_O)         = 3.568d-4*zs(jz2)*ntot !O is fully neutral
+    x(KROME_idx_D)         = 3d-5*ntot
 
     call krome_set_Semenov_Tdust((krome_redshift+1d0)*2.73d0)
     Av = 0.0
